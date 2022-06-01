ee70b520bea7ac1a49565ff279bb8e41cc0c53db
KazharAlyu <85399207+kazharalyu@users.noreply.github.com>
2022-06-01 08:11:23 +0700

    spamer.sh
    ok we try

---
+++ spam.sh
@@ -0,0 +1,8 @@
+pkg update && pkg upgrade
+pkg install git
+pkg install php
+git clone https://github.com/kumpulanremaja/spam-sms.git
+cd spam-sms
+dir
+sleep 1
+to use this, cd spam-sms php "name.php"
